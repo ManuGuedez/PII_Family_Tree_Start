52631eab56ed651c6279d66d3c7085af79113bd0
Manuela Guedez <manuela.guedez@correo.ucu.edu.uy>
2023-10-30 09:41:04 -0300

    Node changes

--- src/Library/Person.cs
+++ src/Library/Person.cs
@@ -6,17 +6,10 @@ namespace Library
     {
         public string Name { get; private set; }
         public int Age { get; private set; }
-        public List<Person> Children { get; private set; }
         public Person(string name, int age)
         {
             this.Name = name;
             this.Age = age;
-            this.Children = new List<Person>();
-        }
-
-        public void AddChildren(Person child)
-        {
-            this.Children.Add(child);
         }
     }
 }
